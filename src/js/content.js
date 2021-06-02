cb83cf749be550c81e38648da60ffc90f8477962
hikarock <i@hika69.com>
2021-06-02 22:56:57 +0900

    Amazon の判定を入れる

--- src/js/content.js
+++ src/js/content.js
@@ -1,18 +1,23 @@
+const isAmazon = () => {
+  return /^www.amazon\.(com|co\.jp)$/.test(location.hostname);
+};
+
+const getMetaDataFromAmazon = () => {
+  const titleSelector = "#productTitle";
+  const titleElm = document.querySelector(titleSelector);
+  const title = titleElm !== null ? titleElm.textContent.trim() : "";
+  const authorsSelector = ".author .a-link-normal";
+  const authorsElm = document.querySelector(authorsSelector);
+  const authors = authorsElm !== null ? authorsElm.textContent.trim() : "";
+  return { title, authors };
+};
+
 chrome.runtime.onMessage.addListener(({ type }, _, sendResponse) => {
   if (type !== "fetchMetaData") {
     return;
   }
-  const titleSelector = "#productTitle";
-  const titleElm = document.querySelector(titleSelector);
-  let title = "";
-  if (titleElm !== null) {
-    title = titleElm.textContent.trim();
-  }
-  const authorsSelector = ".author .a-link-normal";
-  const authorsElm = document.querySelector(authorsSelector);
-  let authors = "";
-  if (authorsElm !== null) {
-    authors = authorsElm.textContent.trim();
-  }
-  sendResponse({ title, authors });
+  const metaData = isAmazon()
+    ? getMetaDataFromAmazon()
+    : { title: "", authors: "" };
+  sendResponse(metaData);
 });
