405f62870b089af9afedfcf4646774ac32969778
hikarock <i@hika69.com>
2021-12-25 23:53:16 +0900

    URLの登録プロパティを変更する

--- src/js/popup.js
+++ src/js/popup.js
@@ -30,7 +30,6 @@ const buildPayload = ({
           {
             text: {
               content: title,
-              link: { url },
             },
           },
         ],
@@ -60,6 +59,9 @@ const buildPayload = ({
           },
         ],
       },
+      URL: {
+        url,
+      },
     },
   });
 };
@@ -78,6 +80,7 @@ const inputTitleElm = document.getElementById("title");
 const inputAuthorsElm = document.getElementById("authors");
 const inputPublisherElm = document.getElementById("publisher");
 const inputMediaTypeElm = document.getElementById("media-type");
+const inputUrlElm = document.getElementById("url");
 const processingElm = document.getElementById("processing");
 const successElm = document.getElementById("success");
 const errorElm = document.getElementById("error");
@@ -89,6 +92,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     inputAuthorsElm.value = payload?.authors ? payload.authors : "";
     inputPublisherElm.value = payload?.publisher ? payload.publisher : "";
     url = payload?.url ? payload.url : "";
+    inputUrlElm.value = url;
     cover = payload?.cover ? payload.cover : "";
     coverElm.setAttribute("src", cover);
   });
